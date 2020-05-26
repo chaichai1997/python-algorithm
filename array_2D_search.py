fd50e45dcf97852157796d3d7860a4926f98b9a0
ChaiChai <1224816105@qq.com>
2020-05-26 19:40:22 +0800

    array_2D_search

--- array_2D_search.py
+++ array_2D_search.py
@@ -3,6 +3,36 @@
 
 
 """
-实现二维有序数组的
-
+实现二维有序数组的高效查找
+a[i,j]
+if a[i][j]>data, j=j-1
+if a[i][j]<data, i = i+1
 """
+
+
+def search_2D(array, data):
+    if array is None:
+        return False
+    i = 0
+    r = len(array)
+    c = len(array[0])
+    j = c-1
+    while i < r and j >= 0:
+        if array[i][j] == data:
+            return True
+        elif array[i][j] >= data:
+            j -= 1
+        else:
+            i += 1
+    return False
+
+
+if __name__ == '__main__':
+    array=[
+        [1, 2, 3],
+        [3, 4, 5],
+        [4, 7, 9]
+    ]
+    data = 9
+    print(search_2D(array, data))
+
