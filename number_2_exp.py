bbdb4820ff43b75bc650c9d4ae18ae850a7e991f
ChaiChai <1224816105@qq.com>
2020-06-05 16:12:10 +0800

    number_power_and

--- number_2_exp.py
+++ number_2_exp.py
@@ -24,12 +24,36 @@ def is_power(n):
     return False
 
 
+"""
+与操作
+若 n是 2的次方
+则 其二进制为 00001000
+n-1 00000111
+与操作结果为0
+"""
+
+
+def is_power_2(n):
+    if n < 1:
+        return False
+    m = n & (n-1)
+    return m == 0
+
+
 if __name__ == '__main__':
     if is_power(8):
         print("8 is 2's power")
     else:
         print("8 isn't 2's power")
     if is_power(15):
+        print("15 is 2's power")
+    else:
+        print("15 isn't 2's power")
+    if is_power_2(8):
+        print("8 is 2's power")
+    else:
+        print("8 isn't 2's power")
+    if is_power_2(15):
         print("15 is 2's power")
     else:
         print("15 isn't 2's power")
