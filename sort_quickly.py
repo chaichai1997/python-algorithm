e4d9947ded28529afd92f331ac1e2ca6c07c6d1b
ChaiChai <1224816105@qq.com>
2020-06-09 15:23:58 +0800

    sort_quickly

--- sort_quickly.py
+++ sort_quickly.py
@@ -4,29 +4,30 @@
 
 """
 快速排序
-基本原理：对于给定的一组记录，第一轮选择最小(大)值，与第一条记录进行交换。
-然后从剩余元素中，找出最小(大)值，与第二条记录进行交换，知道记录中未排序元素个数为0.
-时间复杂度：O（n * n ）
+采用分治的方法，通过一次排序，将序列切分为两部分，前部分的记录均比后部分的记录小。
+然后再按此方法对这两部分数据分别进行快速排序，递归实现。
 """
 
 
-def sort_quickly(data):
-    n = len(data)
-    for i in range(n):
-        tmp = i
-        for j in range(i+1, n):
-            if data[tmp] > data[j]:
-                tmp = j
-        max = data[tmp]
-        data[tmp] = data[i]
-        data[i] = max
-        print(data)
+def sort_quickly(data, left, right):
+    if left >= right:
+        return data
+    key = data[left]
+    low = left
+    high = right
+    while left < right:
+        while left < right and data[right] >= key:
+            right -= 1
+        data[left] = data[right]
+        while left < right and data[left] <= key:
+            left += 1
+        data[right] = data[left]
+    data[right] = key
+    sort_quickly(data, low, left-1)
+    sort_quickly(data, left+1, high)
+    return data
 
 
 if __name__ == '__main__':
     lists = [3, 4, 2, 8, 9, 5, 1]
-    sort_quickly(lists)
-
-
-
-
+    print(sort_quickly(lists, 0, len(lists)-1))
